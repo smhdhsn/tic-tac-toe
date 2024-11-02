909fa041b51b39ae393f1c4b07d0206ed0e0631a
Mahdi Hasani <mahdi.hasani@urbansportsclub.com>
2024-11-02 16:59:58 +0100

    read user's marker from input

---
+++ inputs/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module contains the functions for getting inputs from user.
+"""
+
+from .stdin import read_player_marker
+
+__all__ = [
+    'read_player_marker'
+]
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Entry point for the ticktacktoe application.
 """
 
 from samples import create_empty_board
+from inputs import read_player_marker
 from outputs import print_board
 
 def main():
@@ -11,6 +12,8 @@ def main():
     """
 
     initial_state = create_empty_board()
+
+    max_player = read_player_marker("Choose your marker (X or O):")
 
     print_board(initial_state)
 
