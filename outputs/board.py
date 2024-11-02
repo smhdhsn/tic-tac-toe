bdb7c3e0b7a6f326f716d98285705540d84b296a
Mahdi Hasani <mahdi.hasani@urbansportsclub.com>
2024-11-02 19:18:21 +0100

    include row and column numbers

--- outputs/board.py
+++ outputs/board.py
@@ -14,12 +14,13 @@ def print_board(board: List[List[Mark]]) -> None:
     """
 
     print(
-        "┌───┬───┬───┐",
-        f"│ {board[0][0].value} │ {board[0][1].value} │ {board[0][2].value} │",
-        "├───┼───┼───┤",
-        f"│ {board[1][0].value} │ {board[1][1].value} │ {board[1][2].value} │",
-        "├───┼───┼───┤",
-        f"│ {board[2][0].value} │ {board[2][1].value} │ {board[2][2].value} │",
-        "└───┴───┴───┘",
+        "     0   1   2  ",
+        "   ┌───┬───┬───┐",
+        f" 0 │ {board[0][0].value} │ {board[0][1].value} │ {board[0][2].value} │",
+        "   ├───┼───┼───┤",
+        f" 1 │ {board[1][0].value} │ {board[1][1].value} │ {board[1][2].value} │",
+        "   ├───┼───┼───┤",
+        f" 2 │ {board[2][0].value} │ {board[2][1].value} │ {board[2][2].value} │",
+        "   └───┴───┴───┘",
         sep="\n"
     )
