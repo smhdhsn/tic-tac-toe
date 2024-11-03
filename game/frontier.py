91de6dbe9d29759cc7c304e2c38ca53ce0864c96
Mahdi Hasani <mahdi.hasani@urbansportsclub.com>
2024-11-03 01:26:48 +0100

    include frontier to manage the game

--- game/frontier.py
+++ game/frontier.py
@@ -9,18 +9,30 @@ class Frontier:
     A container for managing states and exploration of states.
     """
 
-    def __init__(self, board: Board, max_player: Player) -> None:
+    def __init__(self, board: Board, max_player: Player, min_player: Player) -> None:
         """
         Initialize the Frontier with board and max player.
 
         Args:
-            board (Board): The board of the tic-tac-toe game.
+            board (Board): The board of the Tic-Tac-Toe game.
             max_player (Player): The max player.
+            min_player (Player): The min player.
 
         Raises:
             ValueError: If the provided board is not an instance of Board.
             ValueError: If the provided max_player is not an instance of Player.
+            ValueError: If the provided min_player is not an instance of Player.
         """
+
+        if not isinstance(board, Board):
+            raise ValueError("Provided board has to be an instance of Board.")
+
+        if not isinstance(max_player, Player):
+            raise ValueError("Provided max player has to be an instance of Player.")
+
+        if not isinstance(min_player, Player):
+            raise ValueError("Provided min player has to be an instance of Player.")
 
         self.board: Board = board
         self.max_player: Player = max_player
+        self.min_player: Player = min_player
--- main.py
+++ main.py
@@ -2,25 +2,30 @@
 Entry point for the Tic-Tac-Toe application.
 """
 
+from inputs import read_human_player_mark
 from samples import create_empty_board
-from inputs import read_player_mark
-from outputs import print_board
-from models import Player
+from helper import get_opposite_mark
+from models import Human, AI
+from game import Frontier
 
 def main():
     """
     Main function to prepare and begin the game.
     """
 
-    initial_state = create_empty_board()
+    initial_board = create_empty_board()
 
-    player_mark = read_player_mark("Choose your mark (X or O): ")
+    human_mark = read_human_player_mark("Choose your mark (X or O): ")
+    max_player = Human(human_mark)
 
-    max_player = Player(player_mark)
+    ai_mark = get_opposite_mark(human_mark)
+    min_player = AI(ai_mark)
 
-    initial_state.set_mark(1, 1, max_player.get_mark())
-
-    print_board(initial_state)
+    frontier = Frontier(
+        board=initial_board,
+        max_player=max_player,
+        min_player=min_player
+    )
 
 if __name__ == "__main__":
     main()
