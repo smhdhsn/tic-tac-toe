1b763a6cfd2d3692193f853d4d3891a43c2f8d09
Mahdi Hasani <mahdi.hasani@urbansportsclub.com>
2024-11-23 14:38:12 +0100

    calculate state space considering x and o players can both start the game

--- app/state_space/main.py
+++ app/state_space/main.py
@@ -6,20 +6,22 @@ from pickle import dump
 from models import State, Board, Mark
 from search import create_state_space
 
+from helpers import print_board
+
 
 def main():
     """
     Main function to prepare and begin the calculation.
     """
 
-    default_mark: Mark = Mark.X
     empty_board: Board = Board()
     initial_state: State = State(empty_board)
 
-    create_state_space(
-        initial_state,
-        default_mark,
-    )
+    for starting_player in [Mark.X, Mark.O]:
+        create_state_space(
+            initial_state,
+            starting_player,
+        )
 
     with open("export/state_space.pkl", "wb") as file:
         dump(initial_state, file)
