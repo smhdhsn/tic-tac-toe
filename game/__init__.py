552ea69d0bd704700d56f5ec7d2ed0c62764e196
Mahdi Hasani <mahdi.hasani@urbansportsclub.com>
2024-12-08 18:59:25 +0100

    implement minimax algorithm

--- game/__init__.py
+++ game/__init__.py
@@ -2,8 +2,15 @@
 This module contains the functionalities for the game.
 """
 
-from .rules import get_empty_cells, get_turn_mark, is_board_full, winner_check
+from .rules import get_empty_cells, get_turn_mark, is_board_full, winner_check, utility
 from .algorithm import run
 
 # Expose methods at the package level.
-__all__ = ["get_empty_cells", "get_turn_mark", "is_board_full", "winner_check", "run"]
+__all__ = [
+    "get_empty_cells",
+    "get_turn_mark",
+    "is_board_full",
+    "winner_check",
+    "utility",
+    "run",
+]
