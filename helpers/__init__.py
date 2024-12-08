644247947712e6d3fd07ecec53ae952a38d58068
Mahdi Hasani <mahdi.hasani@urbansportsclub.com>
2024-12-08 16:10:55 +0100

    improve structure for outputting the board

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -2,9 +2,8 @@
 This module contains helper functions for the game.
 """
 
-from .announce import announce_result
 from .mark import decide_marks
-from .board import print_board
+from .announce import announce_result
 
 # Expose methods at the package level.
-__all__ = ["announce_result", "decide_marks", "print_board"]
+__all__ = ["decide_marks", "announce_result"]
