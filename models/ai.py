ec4348dc45481877b35c2cad573012fbfdc165d5
Mahdi Hasani <mahdi.hasani.eng@gmail.com>
2026-04-14 20:50:31 +0200

    use state space in ai agent class

--- models/ai.py
+++ models/ai.py
@@ -2,15 +2,56 @@
 This package is the ai player in the game.
 """
 
+from pickle import load
+from search import create_state_space
 from dto import Position
 from .player import Player
 from .board import Board
+from .state import State
+from .mark import Mark
 
 
 class AI(Player):
     """
     This class holds the functionalities for the ai player in the Tic-Tac-Toe game.
     """
+
+    def __init__(self, mark: Mark) -> None:
+        """
+        Initializes the AI player with a specific mark and loads the state space.
+
+        Args:
+            mark (Mark): The mark assigned to the AI player.
+        """
+
+        super().__init__(mark)
+        self.state_space: State = None
+        self.load_state_space()
+
+    def load_state_space(self) -> None:
+        """
+        Loads the state space from a pickled file for the AI to reference during gameplay.
+
+        Raises:
+            FileNotFoundError: If the state space file is not found.
+            Exception: If there is an error during the loading process.
+        """
+
+        if self.state_space is None:
+            try:
+                with open("export/state_space.pkl", "rb") as file:
+                    self.state_space = load(file)
+
+            except Exception:
+                initial_state = Board()
+                initial_state: State = State(initial_state)
+
+                create_state_space(
+                    mark=self.get_mark(),
+                    state=initial_state,
+                )
+
+                self.state_space = initial_state
 
     def get_next_move(self, board: Board) -> Position:
         """
