6edad23e856708769691e95827de8c7f7afdfa63
Mahdi Hasani <mahdi.hasani.eng@gmail.com>
2026-04-14 20:50:24 +0200

    improve structure

---
+++ helpers/__init__.py
@@ -0,0 +1,10 @@
+"""
+This module contains helper functions for the game.
+"""
+
+from .inputs import decide_marks
+
+# Expose methods at the package level.
+__all__ = [
+    'decide_marks'
+]
--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 Entry point for the Tic-Tac-Toe application.
 """
 
-from inputs import read_human_player_mark
-from samples import create_empty_board
-from helper import get_opposite_mark
-from models import Human, AI
+from models import Board, Human, AI
+from helpers import decide_marks
 from game import Frontier
 
 def main():
@@ -13,12 +11,11 @@ def main():
     Main function to prepare and begin the game.
     """
 
-    initial_board = create_empty_board()
+    initial_board = Board()
 
-    human_mark = read_human_player_mark("Choose your mark (X or O): ")
+    human_mark, ai_mark = decide_marks("Choose your mark (X or O): ")
+
     max_player = Human(human_mark)
-
-    ai_mark = get_opposite_mark(human_mark)
     min_player = AI(ai_mark)
 
     frontier = Frontier(
@@ -26,6 +23,8 @@ def main():
         max_player=max_player,
         min_player=min_player
     )
+
+    frontier.board.print()
 
 if __name__ == "__main__":
     main()
