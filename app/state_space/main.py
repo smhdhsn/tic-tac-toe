23bb7b106ec8c5a1a78a9c26d70eea70c555f56f
Mahdi Hasani <mahdi.hasani.eng@gmail.com>
2026-04-14 20:50:30 +0200

    include state_space entry point to calculate state space for agent

---
+++ app/state_space/main.py
@@ -0,0 +1,13 @@
+"""
+Entry point for calculating state space for the agent in the Tic-Tac-Toe application.
+"""
+
+
+def main():
+    """
+    Main function to prepare and begin the calculation.
+    """
+
+
+if __name__ == "__main__":
+    main()
