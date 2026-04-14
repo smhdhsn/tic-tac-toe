27ea889810b684706b9d1fced200b1011227832e
Mahdi Hasani <mahdi.hasani.eng@gmail.com>
2026-04-14 20:50:18 +0200

    module for creating initial state

---
+++ samples/__init__.py
@@ -0,0 +1,10 @@
+"""
+Package for creating sample problems to work on.
+"""
+
+from .board import create_empty_board
+
+# Expose methods at the package level.
+__all__ = [
+    'create_empty_board'
+]
