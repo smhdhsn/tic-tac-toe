ecff1e698e84b7bca80b5d513cddbabc431e0f5b
Mahdi Hasani <mahdi.hasani.eng@gmail.com>
2026-04-14 20:50:29 +0200

    organize application's entry points

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+"""
+The `app` package contains various entry points for the application.
+
+Subpackages:
+- tic_tac_toe: The game entry point.
+"""
---
+++ app/tic_tac_toe/__init__.py
@@ -0,0 +1,6 @@
+"""
+This package contains the main entry point for the Tic-Tac-Toe game.
+
+This package is responsible for bootstrapping the application's runtime 
+logic and integrating its components.
+"""
